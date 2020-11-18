9f8eab1b979a67ed4b76f16b8c627732124ef333
Daniel Choy <danieljinchoy@gmail.com>
2020-11-18 17:52:32 -0500

    organize files

--- ames_housing/ml_project.R
+++
@@ -1,9 +0,0 @@
-library(dplyr)
-library(ggplot2)
-library(lubridate)
-library(tidyverse)
-
-housing = read.csv('Ames_HousePrice.csv', header = TRUE)
-real_estate = read.csv('data/Ames_Real_Estate_Data.csv')
-
-log(0.1)
